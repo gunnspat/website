af2e126d5e0581cf32522d1516078be8f058fe01
gunnspat <gunnspat@hotmail.co.uk>
2022-02-12 12:52:11 +0000

    website update

--- _pgexport/footer.php
+++ _pgexport/footer.php
@@ -1,12 +1,4 @@
 
-        </main>
-        <footer>
-            <div class="panel-footer">
-                <?php _e( 'Gunn&apos;s Portable Appliance Testing : 2010-2021&nbsp;', 'gunnspat' ); ?>
-                <i class="fa-copyright fa"></i> 
-                <?php _e( 'All Rights Reserved.', 'gunnspat' ); ?>
-            </div>
-        </footer>
         <!-- Bootstrap core JavaScript
     ================================================== -->
         <!-- Placed at the end of the document so the pages load faster -->
--- _pgexport/index.php
+++ _pgexport/index.php
@@ -1,25 +1,129 @@
 <?php get_header(); ?>
 
-<?php if ( have_posts() ) : ?>
-    <?php while ( have_posts() ) : the_post(); ?>
-        <?php PG_Helper::rememberShownPost(); ?>
-        <div <?php post_class( 'container' ); ?> id="post-<?php the_ID(); ?>">
-            <div class="row">
-                <div class="col-md-9 col-lg-9 col-sm-12 col-xs-12">
-                    <?php the_content(); ?>
+        <div id="fb-root"></div>
+        <div class="header hdsml">
+            <div class="logo">
+                <h1 class="headertxt hidden-xs hidden-sm"><?php _e( 'For all your PAT Testing needs call - 07881688784', 'gunnspat' ); ?></h1>
+                <h3 class="headertxt hidden-xs hidden-md hidden-lg"><?php _e( 'For all your PAT Testing needs call - 07881688784', 'gunnspat' ); ?></h3>
+                <h5 class="headersml hidden-md hidden-lg hidden-sm"><?php _e( 'Call Us - 07881688784', 'gunnspat' ); ?></h5>
+            </div>
+        </div>
+        <nav class="navbar navbar-default" role="navigation">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only"><?php _e( 'Toggle navigation', 'gunnspat' ); ?></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
                 </div>
-                <div class="col-md-3 col-lg-3 hidden-sm hidden-xs">
-                    <div class="fb-page" data-href="https://www.facebook.com/gunnspat/" data-tabs="timeline,messages" data-width="350px" data-height="600px" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
-                        <blockquote cite="https://www.facebook.com/gunnspat/" class="fb-xfbml-parse-ignore">
-                            <a href="https://www.facebook.com/gunnspat/"><?php _e( 'Gunn&#039;s Portable Appliance Testing', 'gunnspat' ); ?></a>
-                        </blockquote>
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <div class="navbar-left hidden-xs">
+                        <i class="fa fa-cc-paypal fa-2x fa-inverse"></i>
+                    </div>
+                    <ul class="nav navbar-nav navbar-centre">
+                        <li>
+                            <a href="index.html"><?php _e( 'Home', 'gunnspat' ); ?></a>
+                        </li>
+                        <li>
+                            <a href="about.html"><?php _e( 'About Us', 'gunnspat' ); ?></a>
+                        </li>
+                        <li>
+                            <a href="prices.html"><?php _e( 'Prices', 'gunnspat' ); ?></a>
+                        </li>
+                        <li>
+                            <a href="quote.html"><?php _e( 'Quote Me', 'gunnspat' ); ?></a>
+                        </li>
+                        <li class="dropdown">
+</li>
+                    </ul>
+                    <div class="navbar-right hidden-xs">
+                        <a href="https://www.facebook.com/gunnspat" target="_blank"><i class="fa fa-facebook fa-inverse fa-2x"></i></a>
+                        <a href="https://twitter.com/gunnspat_steve" target="_blank"><i class="fa fa-inverse fa-twitter fa-2x"></i></a>
                     </div>
                 </div>
             </div>
-        </div>
-    <?php endwhile; ?>
-<?php else : ?>
-    <p><?php _e( 'Sorry, no posts matched your criteria.', 'gunnspat' ); ?></p>
-<?php endif; ?>
+        </nav>
+        <main>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-9 col-lg-9 col-sm-12 col-xs-12">
+                        <h2 class="hidden-xs" style="bottom: 6px;
+    right: 6px;
+    left: 6px;
+    top: 6px;"><?php _e( 'Welcome to Gunn\'s Portable Appliance Testing.', 'gunnspat' ); ?></h2>
+                        <h3 class="hidden-sm hidden-md hidden-lg"><?php _e( 'Welcome to Gunn\'s Portable Appliance Testing.', 'gunnspat' ); ?></h3>
+                        <p><?php _e( 'Portable Appliance Testing is where electrical appliances are routinely checked to see whether they are safe for continued use within the workplace.', 'gunnspat' ); ?></p>
+                        <p><?php _e( 'PAT testing is vital to ensure the protection for your business and more importantly customers and employees.', 'gunnspat' ); ?></p>
+                        <p><?php _e( 'Appliances covered is anything fitted with a BS1363 plug, including PCs, Kettles, Irons, Televisions, Toasters, Microwaves, Power Tools, Washing Machines, Vacuum Cleaners etc.... or anything that can be moved whilst connected to the electricity supply.', 'gunnspat' ); ?></p>
+                        <h3 class="hidden-xs textblue"><?php _e( 'Some of the key benefits of using Gunn\'s PAT Testing:', 'gunnspat' ); ?></h3>
+                        <h4 class="hidden-sm hidden-md hidden-lg textblue"><?php _e( 'Some of the key benefits of using Gunn\'s PAT Testing:', 'gunnspat' ); ?></h4>
+                        <ul style="list-style: none;">
+                            <li>
+                                <i class="fa fa-check-square-o"></i>
+                                <?php _e( 'No Call Out Fee\'s', 'gunnspat' ); ?>
+                            </li>
+                            <li>
+                                <i class="fa fa-check-square-o"></i>
+                                <?php _e( 'PAT Testers Calibrated Annually', 'gunnspat' ); ?>
+                            </li>
+                            <li>
+                                <i class="fa fa-check-square-o"></i>
+                                <?php _e( 'Free Reminder Service', 'gunnspat' ); ?>
+                            </li>
+                            <li>
+                                <i class="fa fa-check-square-o"></i>
+                                <?php _e( 'Paperwork & Digital Test Results within 48hrs (PAT Test Pack)', 'gunnspat' ); ?>
+                            </li>
+                            <li>
+                                <i class="fa fa-check-square-o"></i>
+                                <?php _e( 'Appliances Correctly Labelled (Pass / Fail)', 'gunnspat' ); ?>
+                            </li>
+                            <li>
+                                <i class="fa fa-check-square-o"></i>
+                                <?php _e( 'Free Replacement Fuses (If Required)', 'gunnspat' ); ?>
+                            </li>
+                            <li>
+                                <i class="fa fa-check-square-o"></i>
+                                <?php _e( 'Free Replacement Plugs (If Required)', 'gunnspat' ); ?>
+                            </li>
+                            <li>
+                                <i class="fa fa-check-square-o"></i>
+                                <?php _e( 'Free Minor Repairs and Re-Test', 'gunnspat' ); ?>
+                            </li>
+                            <li>
+                                <i class="fa fa-check-square-o"></i>
+                                <?php _e( '110v & 230/240v Appliances Tested', 'gunnspat' ); ?>
+                            </li>
+                            <li>
+                                <i class="fa fa-check-square-o"></i>
+                                <?php _e( 'Staff Have Paypal Card Readers, Making Payments Easier and Secure', 'gunnspat' ); ?>
+                            </li>
+                        </ul>
+                        <h2 class="hidden-xs textblue"><center>
+                                <?php _e( 'For a Genuine PAT Testing service you can trust... CALL NOW......', 'gunnspat' ); ?>
+                            </center></h2>
+                        <h3 class="hidden-sm hidden-md hidden-lg textblue"><center>
+                                <?php _e( 'For a Genuine PAT Testing service you can trust... CALL NOW......', 'gunnspat' ); ?>
+                            </center></h3>
+                    </div>
+                    <div class="col-md-3 col-lg-3 hidden-sm hidden-xs">
+                        <div class="fb-page" data-href="https://www.facebook.com/gunnspat/" data-tabs="timeline,messages" data-width="350px" data-height="600px" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
+                            <blockquote cite="https://www.facebook.com/gunnspat/" class="fb-xfbml-parse-ignore">
+                                <a href="https://www.facebook.com/gunnspat/"><?php _e( 'Gunn&#039;s Portable Appliance Testing', 'gunnspat' ); ?></a>
+                            </blockquote>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+        <footer>
+            <div class="panel-footer">
+                <?php _e( 'Gunn&apos;s Portable Appliance Testing : 2010-2021&nbsp;', 'gunnspat' ); ?>
+                <i class="fa-copyright fa"></i> 
+                <?php _e( 'All Rights Reserved.', 'gunnspat' ); ?>
+            </div>
+        </footer>        
 
 <?php get_footer(); ?>
